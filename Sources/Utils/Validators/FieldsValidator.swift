ff51de659ecf38fca40868ff3f6adc8a33f72901
Арман Чархчян <re-mont1@mail.ru>
2022-04-29 22:31:17 +0300

    fix

--- Sources/Utils/Validators/FieldsValidator.swift
+++ Sources/Utils/Validators/FieldsValidator.swift
@@ -35,22 +35,6 @@ public struct FieldsValidator {
 }
 
 extension FieldsValidator: CredentionalValidatorProtocol {
-    public enum CredentionalError: LocalizedError {
-        case notFilled
-        case invalidEmail
-        case passwordsNotMatched
-        
-        public var errorDescription: String? {
-            switch self {
-            case .notFilled:
-                return NSLocalizedString("Заполните все поля", comment: "")
-            case .invalidEmail:
-                return NSLocalizedString("Формат почты не является допустимым", comment: "")
-            case .passwordsNotMatched:
-                return NSLocalizedString("Пароли не совпадают", comment: "")
-            }
-        }
-    }
     
     public func checkEmptyRegistration(email: String, password: String, confirmPassword: String) -> Bool {
         return !(email == "" || password == "" || confirmPassword == "")
@@ -72,26 +56,6 @@ extension FieldsValidator: CredentionalValidatorProtocol {
 }
 
 extension FieldsValidator: ProfileValidatorProtocol {
-    
-    public enum ProfileError: LocalizedError {
-        case notFilled
-        case photoNotAdded
-        case ageLess16
-        case ageNotValid
-        
-        public var errorDescription: String? {
-            switch self {
-            case .notFilled:
-                return NSLocalizedString("Заполните все поля", comment: "")
-            case .photoNotAdded:
-                return NSLocalizedString("Вы не добавили фото", comment: "")
-            case .ageLess16:
-                return NSLocalizedString("Вам нет 16-ти лет", comment: "")
-            case .ageNotValid:
-                return NSLocalizedString("Пожалуйста, введите корректную дату рождения", comment: "")
-            }
-        }
-    }
     
     public func validateSelectedAgeNoLess16(date: String) -> Bool {
         guard let age = Int(DateFormatService().getAge(date: date)) else { return false }
@@ -115,5 +79,45 @@ extension FieldsValidator: ProfileValidatorProtocol {
                                 country: String,
                                 city: String) -> Bool {
         return !(username == "" || info == "" || sex == "" || birthday == "" || country == "" || city == "")
+    }
+}
+
+public enum ValidationError {
+
+    public enum Profile: LocalizedError {
+        case notFilled
+        case photoNotAdded
+        case ageLess16
+        case ageNotValid
+        
+        public var errorDescription: String? {
+            switch self {
+            case .notFilled:
+                return NSLocalizedString("Заполните все поля", comment: "")
+            case .photoNotAdded:
+                return NSLocalizedString("Вы не добавили фото", comment: "")
+            case .ageLess16:
+                return NSLocalizedString("Вам нет 16-ти лет", comment: "")
+            case .ageNotValid:
+                return NSLocalizedString("Пожалуйста, введите корректную дату рождения", comment: "")
+            }
+        }
+    }
+    
+    public enum Credentionals: LocalizedError {
+        case notFilled
+        case invalidEmail
+        case passwordsNotMatched
+        
+        public var errorDescription: String? {
+            switch self {
+            case .notFilled:
+                return NSLocalizedString("Заполните все поля", comment: "")
+            case .invalidEmail:
+                return NSLocalizedString("Формат почты не является допустимым", comment: "")
+            case .passwordsNotMatched:
+                return NSLocalizedString("Пароли не совпадают", comment: "")
+            }
+        }
     }
 }
